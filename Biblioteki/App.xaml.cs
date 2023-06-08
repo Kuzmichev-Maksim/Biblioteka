ed9eb4bdd87a250d4c9043dcf1b7a37f84810d92
Kuzmichev-Maksim <Kuzmichev-Maksim>
2023-06-08 20:59:18 +0300

    Библиотека

---
+++ Biblioteki/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Biblioteki
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static string theme;
+
+
+        public static string Theme
+        {
+            get { return theme; }
+            set
+            {
+                theme = value;
+                var dict = new ResourceDictionary { Source = new Uri($"C:/Users/maksi/OneDrive/Документы/Колледж/4 семестр/C#/практическая 8 - Библиотеки/Biblioteki/WpfCustomControlLibrary1/Themes/{value}.xaml", UriKind.RelativeOrAbsolute) };
+                Current.Resources.MergedDictionaries.RemoveAt(0);
+                Current.Resources.MergedDictionaries.Insert(0, dict);
+            }
+        }
+    }
+}
